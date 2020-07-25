9fca4717b4111909542e67793f78b0901f3e659d
Bipin Thite <bipinthite@gmail.com>
2020-07-25 15:51:12 +0530

    refactor: Upgrade to Spring Boot v2.3.2, Java 11

--- src/main/java/com/example/app/controller/AppController.java
+++ src/main/java/com/example/app/controller/AppController.java
@@ -2,6 +2,7 @@ package com.example.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.app.service.AppService;
 
@@ -9,7 +10,8 @@ import com.example.app.service.AppService;
  * @author https://github.com/ibipn
  *
  */
-@RestController("/api")
+@RestController
+@RequestMapping("/v1/hello")
 public class AppController {
 
   @Autowired
